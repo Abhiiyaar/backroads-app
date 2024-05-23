8128d2017b486f7a8e60be57c55d5a44572274f5
Abhiiyaar <itsabhigandhi@gmail.com>
2024-05-23 13:18:44 +0530

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,109 @@
+import tour1 from '../images/tour-1.jpeg'
+import tour2 from '../images/tour-2.jpeg'
+import tour3 from '../images/tour-3.jpeg'
+import tour4 from '../images/tour-4.jpeg'
+
+export const pageLinks = [
+  {
+    id: 1,
+    href: "#home",
+    text: "home",
+  },
+  {
+    id: 2,
+    href: "#about",
+    text: "about",
+  },
+  {
+    id: 3,
+    href: "#services",
+    text: "services",
+  },
+  {
+    id: 4,
+    href: "#tours",
+    text: "tours",
+  },
+];
+
+export const socialLinks = [
+  {
+    id: 1,
+    href: "https://www.instagram.com/abhiiyaar/",
+    icon: "fab fa-instagram",
+  },
+  {
+    id: 2,
+    href: "https://x.com/abhiiyaar",
+    icon: "fab fa-twitter",
+  },
+  {
+    id: 3,
+    href: "https://www.linkedin.com/in/abhiiyaar/",
+    icon: "fab fa-linkedin",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    title: "saving money",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+    icon: "fas fa-wallet fa-fw",
+  },
+  {
+    id: 2,
+    title: "endless hiking",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+    icon: "fas fa-tree fa-fw",
+  },
+  {
+    id: 3,
+    title: "amazing comfort",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+    icon: "fas fa-socks fa-fw",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "jan 26th, 2024",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "feb 26th, 2024",
+    title: "best of java",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "indonesia",
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "march 26th, 2024",
+    title: "explore hong kong",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "hong kong",
+    duration: 8,
+    cost: 5000,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "april 26th, 2024",
+    title: "kenya highlights",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "kenya",
+    duration: 20,
+    cost: 3300,
+  },
+];
